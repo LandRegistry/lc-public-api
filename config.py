55380843f9741f9be8bc6466b66279194b048fc7
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-23 14:44:09 +0100

    Add separate config for preview.

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
+
 class Config(object):
     DEBUG = False
 
-class DevelopmentConfig(object):
+
+class DevelopmentConfig(Config):
     DEBUG = True
+    B2B_PROCESSOR_URL = "http://localhost:5002"
+
+
+class PreviewConfig(Config):
     B2B_PROCESSOR_URL = "http://localhost:5002"
