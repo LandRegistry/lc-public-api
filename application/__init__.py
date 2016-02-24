eb7d1e8ccede9f225cd3e23ce4db75a2259a723a
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-02-24 08:41:21 +0000

    Diagnotics

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 import os
 from log.logger import setup_logging
+import logging
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
 setup_logging(app.config)
+
+
+logging.info('================================')
+logging.ingo(os.getenv('AUTOMATIC_PROCESS_URL', 'AUTOMATIC_PROCESS_URL NOT SET'))
+logging.info('================================')
