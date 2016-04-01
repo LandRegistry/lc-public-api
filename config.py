89ced9c28d32b69dad0ba4c76889322a3535da27
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-04-01 12:22:57 +0100

    Switch to single env vars

--- config.py
+++ config.py
@@ -4,8 +4,5 @@ import os
 class Config(object):
     APPLICATION_NAME = 'lc-public-api'
     DEBUG = os.getenv('DEBUG', True)
-    MQ_USERNAME = os.getenv("MQ_USERNAME", "mquser")
-    MQ_PASSWORD = os.getenv("MQ_PASSWORD", "mqpassword")
-    MQ_HOSTNAME = os.getenv("MQ_HOST", "localhost")
-    MQ_PORT = os.getenv("MQ_PORT", "5672")
+    AMQP_URI = os.getenv("AMQP_URI", "amqp://mquser:mqpassword@localhost:5672")
     B2B_PROCESSOR_URL = os.getenv('AUTOMATIC_PROCESS_URL', 'http://localhost:5002')
